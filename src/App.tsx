10a26635ffd17d70d09083626ea5063e536851a3
JAYBORICHA <jayboricha707@gmail.com>
2023-12-21 23:42:03 +0530

    Continued with frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Container, Stack, TextField, Typography } from '@mui/material'
+import { Container, Stack, TextField, Typography, Paper } from '@mui/material'
 
 
 function App() {
@@ -11,18 +11,22 @@ function App() {
   }
 
   return (
-    <Container maxWidth={'xl'} sx={{display : 'flex', alignItems : 'center', justifyContent :'center'}}>
-      <Stack>
-        <Typography sx={{p :1}}>
-          Welcome to the url shortner
-        </Typography>
-        <Stack maxWidth='sm' spacing={2}>
-          <Typography>Enter your URL here</Typography>
-          <TextField label='Enter Url' onChange={(e:React.ChangeEvent<HTMLInputElement>) => (handleChange(e))} value={url}/>
-          <Typography>Shorterned url</Typography>
-          <TextField label='' value={shortUrl}/>
-        </Stack>
-        </Stack>
+    <Container sx={{display : 'flex', alignItems : 'center', justifyContent :'center', height :'100vh', width : '100%' }}>
+      <Paper sx={{p: 4, width : '600px'}}>
+        <Stack>
+          <Stack alignSelf={'center'}>
+            <Typography variant={'h4'} sx={{p :1}}>
+              Welcome to the url shortner
+            </Typography>
+          </Stack>
+          <Stack maxWidth='sm' spacing={2}>
+            <Typography>Enter your URL here</Typography>
+            <TextField label='Enter Url' onChange={(e:React.ChangeEvent<HTMLInputElement>) => (handleChange(e))} value={url}/>
+            <Typography>Shorterned url</Typography>
+            <TextField label='' value={shortUrl}/>
+          </Stack>
+          </Stack>
+        </Paper>
     </Container>
   )
 }
